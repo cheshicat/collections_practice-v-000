89103e270a8b899373e51007527595c17d1b7838
Allison Howard <alliemhow@gmail.com>
2018-09-12 06:02:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,8 +34,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index do |word|
-    puts "#{word}s"
-  end
-  array.join
+  array.each_with_index.collect{|element, index| "#{element}" + 's'}
 end
